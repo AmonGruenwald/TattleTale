18b84d6017ca80ca0e43c99bd18cccea194c0572
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-01-02 16:50:56 +0100

    Add most basic build setup

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Hello World!\n";
+
+    std::cin.get();
+    return EXIT_SUCCESS; // optional return value
+}
