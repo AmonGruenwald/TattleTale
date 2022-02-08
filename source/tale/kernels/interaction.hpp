0d5def40ac73a0808468b4cd085f1f31fd809ebe
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-02-08 19:06:54 +0100

    Define Interaction member variables

--- source/tale/kernels/interaction.hpp
+++ source/tale/kernels/interaction.hpp
@@ -2,6 +2,9 @@
 #define TALE_INTERACTION_H
 
 #include "tale/kernels/kernel.hpp"
+#include "tale/kernels/emotion.hpp"
+#include "tale/kernels/relationship.hpp"
+#include "tale/actor.hpp"
 
 namespace tale
 {
@@ -12,8 +15,18 @@ namespace tale
     class Interaction : public Kernel
     {
     public:
+        size_t participant_count_;
+        std::vector<std::weak_ptr<Actor>> participants_;
+        std::vector<float> resource_effects_;
+        std::vector<std::map<EmotionType, float>> emotion_effects_;
+        // For each actor hold a map which maps to participants a map of relationship type value pais
+        std::vector<std::map<size_t, std::map<RelationshipType, float>>> relationship_effects_;
+
         Interaction(std::string name, std::vector<std::weak_ptr<Kernel>> reasons);
         std::string ToString();
+
+    private:
+        Interaction();
     };
 
 } // namespace tale
