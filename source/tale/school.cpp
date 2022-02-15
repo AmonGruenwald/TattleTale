b46a3bc517edc2cc2d5fe7b95cfb7c393259a80d
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-02-15 12:06:08 +0100

    Add documentation to School

--- source/tale/school.cpp
+++ source/tale/school.cpp
@@ -6,13 +6,16 @@ namespace tale
 {
     School::School(Random &random, const Setting &setting, InteractionStore &interaction_store) : random_(random), setting_(setting), interaction_store_(interaction_store)
     {
-        for (size_t i = 0; i < setting_.actor_count; ++i)
+        size_t actor_count = setting_.actor_count;
+        for (size_t i = 0; i < actor_count; ++i)
         {
             std::cout << "CREATING ACTOR " << i << std::endl;
             std::shared_ptr<Actor> actor(new Actor(random_, setting_, *this, interaction_store_, i));
+            // TODO: not needed after random names are used
             actor->name_ = (std::to_string(i) + " ");
             actors_.push_back(actor);
         }
+
         size_t course_count = setting_.course_count();
         for (size_t i = 0; i < course_count; ++i)
         {
@@ -22,11 +25,14 @@ namespace tale
             courses_.push_back(course);
         }
 
+        size_t slot_count_per_week = setting_.slot_count_per_week();
+        std::vector<uint32_t> random_slot_order;
         for (auto &course : courses_)
         {
             std::cout << "FILLING COURSE " << course.id_ << std::endl;
-            std::vector<uint32_t> random_slot_order;
-            size_t slot_count_per_week = setting_.slot_count_per_week();
+            random_slot_order.clear();
+            // randomly order all slots of the course
+            // TODO: optimize this by shuffling an already filled vector everytime instead of this
             while (random_slot_order.size() < slot_count_per_week)
             {
                 uint32_t random_slot = random_.GetUInt(0, slot_count_per_week - 1);
@@ -39,9 +45,11 @@ namespace tale
             }
 
             size_t slot_index = 0;
+            // Go over everyslot
             while (slot_index < random_slot_order.size())
             {
                 std::vector<uint32_t> slots;
+                // Because Actor have the same course a few times per week use the same group a few times
                 for (size_t i = 0; i < setting_.same_course_per_week && slot_index < random_slot_order.size(); ++i)
                 {
                     slots.push_back(random_slot_order[slot_index]);
@@ -49,15 +57,20 @@ namespace tale
                 }
                 std::vector<std::weak_ptr<Actor>> course_group = FindRandomCourseGroup(course.id_, slots);
 
+                std::cout << "\tSLOTS ";
                 for (size_t i = 0; i < slots.size(); ++i)
                 {
+                    std::cout << slots[i];
+                    if (i != slots.size() - 1)
+                    {
+                        std::cout << ",";
+                    }
+                    std::cout << " ";
                     course.AddToSlot(course_group, slots[i]);
                 }
+
+                std::cout << "FILLED WITH " << course_group.size() << " ACTORS." << std::endl;
             }
-        }
-        for (auto &actor : actors_)
-        {
-            std::cout << actor->name_ << "is enrolled in " << actor->GetFilledSlotsCount() << " Courses." << std::endl;
         }
     }
 
@@ -70,6 +83,7 @@ namespace tale
             current_weekday_ = static_cast<Weekday>((static_cast<int>(current_weekday_) + 1) % 7);
         }
     }
+
     void School::SimulateDay(size_t day, Weekday weekday)
     {
         std::cout << "SIMULATING DAY " << day << " WHICH IS A " << weekday_string[static_cast<int>(weekday)] << std::endl;
@@ -80,7 +94,7 @@ namespace tale
                 size_t slot = WeekdayAndDailyTickToSlot(weekday, i);
                 for (auto &course : courses_)
                 {
-                    auto course_group = course.GetCourseGroupForSlot(slot);
+                    std::vector<std::weak_ptr<Actor>> course_group = course.GetCourseGroupForSlot(slot);
                     for (auto &actor : course_group)
                     {
                         std::vector<std::weak_ptr<Kernel>> reasons;
@@ -105,10 +119,12 @@ namespace tale
             }
         }
     }
+
     void School::FreeTimeTick()
     {
-        // std::cout << "FREETIME TICK " << std::endl;
+        std::cout << "FREETIME TICK " << std::endl;
     }
+
     bool School::IsWorkday(Weekday weekday) const
     {
         if (weekday == Weekday::Saturday || weekday == Weekday::Sunday)
@@ -138,14 +154,13 @@ namespace tale
     std::vector<std::weak_ptr<Actor>> School::FindRandomCourseGroup(size_t course_id, const std::vector<uint32_t> &slots)
     {
         std::vector<std::weak_ptr<Actor>> course_group;
-        size_t current_try = 0;
-        while (course_group.size() < setting_.actor_count && current_try < setting_.actor_count)
+        for (size_t i = 0; i < setting_.actors_per_course; ++i)
         {
             size_t random_actor_index = random_.GetUInt(0, actors_.size() - 1);
 
             size_t current_index_search_try = 0;
             while (
-                (actors_[random_actor_index]->AllSlotsFilled() || actors_[random_actor_index]->IsEnrolledInCourse(course_id) || ActorIsInCourseGroup(actors_[random_actor_index], course_group) || !actors_[random_actor_index]->SlotsEmpty(slots)) && current_index_search_try < actors_.size())
+                (actors_[random_actor_index]->AllSlotsFilled() || actors_[random_actor_index]->IsEnrolledInCourse(course_id) || ActorIsInCourseGroup(actors_[random_actor_index], course_group) || !actors_[random_actor_index]->SlotsEmpty(slots)) && current_index_search_try < setting_.actor_count)
             {
                 random_actor_index += -1;
                 random_actor_index %= actors_.size();
@@ -155,7 +170,6 @@ namespace tale
             {
                 course_group.push_back(actors_[random_actor_index]);
             }
-            ++current_try;
         }
         return course_group;
     }
--- source/tale/school.hpp
+++ source/tale/school.hpp
@@ -24,29 +24,101 @@ namespace tale
     static const char *weekday_string[] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
     /**
-     * @brief School managing actors and courses
+     * @brief School class that is responsible for managing actors and courses during the simulation
+     *
+     * The School class uses a \link Setting Settings \endlink object in its constructor to setup the simulation, instancing the appropriate amount of \link Actor Actors \endlink and \link Course Courses \endlink and then filling said
+     * \link Course Courses \endlink with \link Actor Actors \endlink. Afterwards it can be used to run the simulation.
      */
     class School
     {
     public:
+        /**
+         * @brief Constructor that sets up everything necessayr for the simulation.
+         *
+         * Stores references to a Random and an InteractionStore Object, to be used in the simulation. Uses the passed Setting object to instance actors and courses correctly.
+         */
         School(Random &random, const Setting &setting, InteractionStore &interaction_store);
+
+        /**
+         * @brief Runs the simulation for the passed amount of days.
+         *
+         * Calls the private SimulatedDay function and increases current_day_ and increments current_weekday_.
+         */
         void SimulateDays(size_t days);
 
     private:
-        void SimulateDay(size_t day, Weekday weekday);
-        void FreeTimeTick();
-        bool IsWorkday(Weekday weekday) const;
-        bool ActorIsInCourseGroup(const std::shared_ptr<Actor> &actor, const std::vector<std::weak_ptr<Actor>> &course_group) const;
-        size_t WeekdayAndDailyTickToSlot(Weekday weekday, size_t tick) const;
-        std::vector<std::weak_ptr<Actor>> FindRandomCourseGroup(size_t course_id, const std::vector<uint32_t> &slots);
+        /**
+         * @brief Holds all instanced courses
+         */
         std::vector<Course> courses_;
+        /**
+         * @brief Holds all instanced actors.
+         */
         std::vector<std::shared_ptr<Actor>> actors_;
+        /**
+         * @brief Holds a reference to an instance of the Random object that was passed during construction.
+         */
         Random &random_;
+        /**
+         * @brief Holds a reference to an instance of the Setting object that was passed during construction.
+         */
         const Setting &setting_;
+        /**
+         * @brief Holds a reference to an instance of the InteractionStore object that was passed during construction.
+         */
         InteractionStore &interaction_store_;
+        /**
+         * @brief The current tick of the simulation (increases after each slot or freetime activity has commenced for every actor). This is always the tick that will be simulated next.
+         */
         size_t current_tick = 0;
+        /**
+         * @brief The current day. This is always the day that will be simulated next.
+         */
         size_t current_day_ = 0;
+        /**
+         * @brief The current weekday. This is always the weekday that will be simulated next.
+         */
         Weekday current_weekday_ = Weekday::Monday;
+        /**
+         * @brief Simulates a single day.
+         *
+         * Depending on wether it is a workday or the weekend let's the actors go through different \link Interaction Interactions \endlink.
+         */
+        void SimulateDay(size_t day, Weekday weekday);
+        /**
+         * @brief Simulates a tick where the  \link Actor Actors \endlink have free time.
+         */
+        void FreeTimeTick();
+        /**
+         * @brief Checks wheter the passed Weeekday is a workday or not.
+         *
+         * @return The result of the check.
+         */
+        bool IsWorkday(Weekday weekday) const;
+        /**
+         * @brief Checks wheter the passed Actor is in the passed course group.
+         *
+         * @return The result of the check.
+         */
+        bool ActorIsInCourseGroup(const std::shared_ptr<Actor> &actor, const std::vector<std::weak_ptr<Actor>> &course_group) const;
+        /**
+         * @brief Transforms a weekday and a daily tick to a slot index.
+         *
+         * Slots increase each day. So if we assume there are 6 slots per day, the slot indices for monday are 0, 1, 2, 3, 4, 5 and the slots for tuesday 6, 7, 8, 9, 10, 11.
+         * This transforms a weekday and daily tick index (which in the example above would go from 0-5) to corresponding slot index.
+         *
+         * @return The slot index.
+         */
+        size_t WeekdayAndDailyTickToSlot(Weekday weekday, size_t tick) const;
+        /**
+         * @brief Finds a random group of \link Actor Actors \endlink that could take the Course during those slots.
+         *
+         * Picks random \link Actor Actors \endlink and checks if they do not already have the Course corresponding to the course_id. It furhter makes sure that the \link Actor Actors \endlink
+         * also are free during the passed slots. Because of those conditions it is not guaranteed that a course_group will have the full amount of \link Actor Actors \endlink specified in setting_.
+         *
+         * @return The found group of Actors.
+         */
+        std::vector<std::weak_ptr<Actor>> FindRandomCourseGroup(size_t course_id, const std::vector<uint32_t> &slots);
     };
 
 } // namespace tale
