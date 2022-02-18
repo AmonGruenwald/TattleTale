25636b6a7a3d26c78c54a863f1bd0b48ed5af9d1
AmonShokhinAhmed <32668336+AmonShokhinAhmed@users.noreply.github.com>
2022-02-18 16:54:32 +0000

    Deploying to gh-pages from @ AmonShokhinAhmed/TattleTale@f51a4739070fcc88191d43d7472a5fcea3941cd8 🚀

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['days_5fto_5fsimulate_0',['days_to_simulate',['../structtale_1_1_setting.html#ac632dc09e50030988540c2cc999adc25',1,'tale::Setting']]],
-  ['desired_5fmax_5fstart_5frelationships_5fcount_1',['desired_max_start_relationships_count',['../structtale_1_1_setting.html#a40c4feaeb81b635b1c8f22c54561ccba',1,'tale::Setting']]]
+  ['desired_5fmax_5fstart_5frelationships_5fcount_1',['desired_max_start_relationships_count',['../structtale_1_1_setting.html#a40c4feaeb81b635b1c8f22c54561ccba',1,'tale::Setting']]],
+  ['desired_5fmin_5fstart_5frelationships_5fcount_2',['desired_min_start_relationships_count',['../structtale_1_1_setting.html#a5505f1f77141b2fb7e96273e52fbc077',1,'tale::Setting']]]
 ];
--- html/search/functions_7.js
+++ html/search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['max_5fstart_5frelationships_5fcount_0',['max_start_relationships_count',['../structtale_1_1_setting.html#a717f90e13292976d5332fa443ddcd8df',1,'tale::Setting']]],
-  ['minimum_5fnecessary_5fcourse_5fcount_1',['minimum_necessary_course_count',['../structtale_1_1_setting.html#a60e67e920a23742b4c7f2e15025d3c81',1,'tale::Setting']]]
+  ['min_5fstart_5frelationships_5fcount_1',['min_start_relationships_count',['../structtale_1_1_setting.html#a90f958c2745456208c84727cb6884ff4',1,'tale::Setting']]],
+  ['minimum_5fnecessary_5fcourse_5fcount_2',['minimum_necessary_course_count',['../structtale_1_1_setting.html#a60e67e920a23742b4c7f2e15025d3c81',1,'tale::Setting']]]
 ];
