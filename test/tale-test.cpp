c0c96eaf12522d0b308354c1d9d431a95cc13ea2
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-02-18 13:01:42 +0100

    Fix test for GNU

--- test/tale-test.cpp
+++ test/tale-test.cpp
@@ -475,7 +475,7 @@ TEST_F(TaleActor, CreateActor)
 
 TEST_F(TaleActor, ActorHasInitializedStartingValues)
 {
-    EXPECT_NE(actor_->resource_, nullptr);
+    EXPECT_TRUE(actor_->resource_);
     EXPECT_NE(actor_->emotions_.size(), 0);
     EXPECT_NE(actor_->relationships_.size(), 0);
 }
