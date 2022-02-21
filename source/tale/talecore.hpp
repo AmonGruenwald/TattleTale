9394b0e61b8d572117749f9c59b5d10f4173fd69
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-02-21 17:53:54 +0100

    Change verbose print format

--- source/tale/talecore.hpp
+++ source/tale/talecore.hpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 #ifdef TALE_DEBUG_PRINT_OUTPUT
-#define TALE_DEBUG_PRINT(x) std::cout << "[DEBUG][" << __TIME__ << "]\n" \
-                                      << x << "\n"                       \
+#define TALE_DEBUG_PRINT(x) std::cout << "[ DEBUG ][" << __TIME__ << "]\n" \
+                                      << x << "\n"                         \
                                       << "--------------------------------------------------------------------------------\n"
 #else
 #define TALE_DEBUG_PRINT(x)
 #endif
 
 #ifdef TALE_VERBOSE_PRINT_OUTPUT
-#define TALE_VERBOSE_PRINT(x) std::cout << "[ VERBOSE ][" << __TIME__ << "]\n" \
-                                        << x
+#define TALE_VERBOSE_PRINT(x) std::cout << "[VERBOSE][" << __TIME__ << "]\n" \
+                                        << x << "\n"                         \
+                                        << "--------------------------------------------------------------------------------\n"
 #else
 #define TALE_VERBOSE_PRINT(x)
 #endif
