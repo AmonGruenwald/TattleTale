c91a998f2742bea2c71d217e1a9f0ba4e87bc769
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-02-22 11:07:38 +0100

    Change print macros to use colors

--- source/tale/talecore.hpp
+++ source/tale/talecore.hpp
@@ -1,17 +1,18 @@
-#include <iostream>
+#include "rang.hpp"
 
 #ifdef TALE_DEBUG_PRINT_OUTPUT
-#define TALE_DEBUG_PRINT(x) std::cout << "[ DEBUG ][" << __TIME__ << "]\n" \
-                                      << x << "\n"                         \
-                                      << "--------------------------------------------------------------------------------\n"
+#define TALE_DEBUG_PRINT(x) std::cout << rang::bg::gray << rang::fg::blue << rang::style::bold << "[ DEBUG ][" << __TIME__ << "]" \
+                                      << rang::style::reset << rang::bg::reset << rang::fg::reset << "\n"                         \
+                                      << x << "\n\n"
+
 #else
 #define TALE_DEBUG_PRINT(x)
 #endif
 
 #ifdef TALE_VERBOSE_PRINT_OUTPUT
-#define TALE_VERBOSE_PRINT(x) std::cout << "[VERBOSE][" << __TIME__ << "]\n" \
-                                        << x << "\n"                         \
-                                        << "--------------------------------------------------------------------------------\n"
+#define TALE_VERBOSE_PRINT(x) std::cout << rang::bg::yellow << rang::fg::reset << rang::style::bold << "[VERBOSE][" << __TIME__ << "]" \
+                                        << rang::style::reset << rang::bg::reset << rang::fg::reset << "\n"                            \
+                                        << x << "\n\n"
 #else
 #define TALE_VERBOSE_PRINT(x)
 #endif
