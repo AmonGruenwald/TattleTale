bfe3c3b3da4abd2f597f4b4a122caf41b2146c30
AmonShokhinAhmed <32668336+AmonShokhinAhmed@users.noreply.github.com>
2022-03-19 11:04:58 +0000

    Deploying to gh-pages from @ AmonShokhinAhmed/TattleTale@bfd9ec21d0725cede8c0a0b4df5a60cd9348fa1b 🚀

--- html/search/all_4.js
+++ html/search/all_4.js
@@ -3,9 +3,11 @@ var searchData=
   ['filled_5fslots_5fcount_5f_0',['filled_slots_count_',['../classtattletale_1_1_actor.html#a55e91e17a4b11846f7788551761ab71d',1,'tattletale::Actor']]],
   ['findrandomcoursegroup_1',['FindRandomCourseGroup',['../classtattletale_1_1_school.html#aa53a63585bd143fababcc01443d22c58',1,'tattletale::School']]],
   ['first_5fname_5f_2',['first_name_',['../classtattletale_1_1_actor.html#a6b60b12786a000fb2e0b9a3aaefe2054',1,'tattletale::Actor']]],
-  ['formatter_3c_20t_2c_20std_3a_3aenable_5fif_5ft_3c_20std_3a_3ais_5fbase_5fof_3c_20tattletale_3a_3akernel_2c_20t_20_3e_3a_3avalue_2c_20char_20_3e_20_3e_3',['formatter&lt; T, std::enable_if_t&lt; std::is_base_of&lt; tattletale::Kernel, T &gt;::value, char &gt; &gt;',['../structfmt_1_1formatter_3_01_t_00_01std_1_1enable__if__t_3_01std_1_1is__base__of_3_01tattletale_12b0be009c05d162385ec2c059161a16f.html',1,'fmt']]],
-  ['formatter_3c_20tattletale_3a_3averb_20_3e_4',['formatter&lt; tattletale::Verb &gt;',['../structfmt_1_1formatter_3_01tattletale_1_1_verb_01_4.html',1,'fmt']]],
-  ['freetime_5factor_5fcount_5',['freetime_actor_count',['../structtattletale_1_1_setting.html#a90b315471203aeccff36bcbc743dccd0',1,'tattletale::Setting']]],
-  ['freetime_5fgroup_5f_6',['freetime_group_',['../classtattletale_1_1_school.html#a914bd0d2d992b909647e49e64b02a3ae',1,'tattletale::School']]],
-  ['freetimetick_7',['FreeTimeTick',['../classtattletale_1_1_school.html#a84730f21ae16254873268ac9081d74f4',1,'tattletale::School']]]
+  ['formatter_3c_20t_2c_20std_3a_3aenable_5fif_5ft_3c_20std_3a_3ais_5fbase_5fof_3c_20tattletale_3a_3aactor_2c_20t_20_3e_3a_3avalue_2c_20char_20_3e_20_3e_3',['formatter&lt; T, std::enable_if_t&lt; std::is_base_of&lt; tattletale::Actor, T &gt;::value, char &gt; &gt;',['../structfmt_1_1formatter_3_01_t_00_01std_1_1enable__if__t_3_01std_1_1is__base__of_3_01tattletale_1a7e8118b6a406855ea5960a98fe738eb.html',1,'fmt']]],
+  ['formatter_3c_20t_2c_20std_3a_3aenable_5fif_5ft_3c_20std_3a_3ais_5fbase_5fof_3c_20tattletale_3a_3akernel_2c_20t_20_3e_3a_3avalue_2c_20char_20_3e_20_3e_4',['formatter&lt; T, std::enable_if_t&lt; std::is_base_of&lt; tattletale::Kernel, T &gt;::value, char &gt; &gt;',['../structfmt_1_1formatter_3_01_t_00_01std_1_1enable__if__t_3_01std_1_1is__base__of_3_01tattletale_12b0be009c05d162385ec2c059161a16f.html',1,'fmt']]],
+  ['formatter_3c_20tattletale_3a_3akerneltype_20_3e_5',['formatter&lt; tattletale::KernelType &gt;',['../structfmt_1_1formatter_3_01tattletale_1_1_kernel_type_01_4.html',1,'fmt']]],
+  ['formatter_3c_20tattletale_3a_3averb_20_3e_6',['formatter&lt; tattletale::Verb &gt;',['../structfmt_1_1formatter_3_01tattletale_1_1_verb_01_4.html',1,'fmt']]],
+  ['freetime_5factor_5fcount_7',['freetime_actor_count',['../structtattletale_1_1_setting.html#a90b315471203aeccff36bcbc743dccd0',1,'tattletale::Setting']]],
+  ['freetime_5fgroup_5f_8',['freetime_group_',['../classtattletale_1_1_school.html#a914bd0d2d992b909647e49e64b02a3ae',1,'tattletale::School']]],
+  ['freetimetick_9',['FreeTimeTick',['../classtattletale_1_1_school.html#a84730f21ae16254873268ac9081d74f4',1,'tattletale::School']]]
 ];
