0dce744fb83e72adc7db1bcddd53c513f07772fc
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-03-22 15:16:17 +0100

    Add stuff back in

--- source/tale/school.cpp
+++ source/tale/school.cpp
@@ -81,49 +81,49 @@ namespace tattletale
                 }
             }
         }
-        // for (auto &actor : actors_)
-        // {
-        //     if (actor->AllSlotsFilled())
-        //     {
-        //         continue;
-        //     }
-        //     for (size_t slot = 0; slot < slot_count_per_week; ++slot)
-        //     {
-        //         if (actor->SlotEmpty(slot))
-        //         {
-        //             for (auto &course : courses_)
-        //             {
-        //                 if (course.SpaceInSlot(slot))
-        //                 {
-        //                     course.AddToSlot(actor, slot);
-        //                     break;
-        //                 }
-        //             }
-        //         }
-        //     }
-        // }
+        for (auto &actor : actors_)
+        {
+            if (actor->AllSlotsFilled())
+            {
+                continue;
+            }
+            for (size_t slot = 0; slot < slot_count_per_week; ++slot)
+            {
+                if (actor->SlotEmpty(slot))
+                {
+                    for (auto &course : courses_)
+                    {
+                        if (course.SpaceInSlot(slot))
+                        {
+                            course.AddToSlot(actor, slot);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
 
-        // for (size_t course_index = 0; course_index < course_count; ++course_index)
-        // {
-        //     for (size_t slot_index = 0; slot_index < slot_count_per_week; ++slot_index)
-        //     {
-        //         for (size_t other_course_index = course_index + 1; other_course_index < course_count; ++other_course_index)
-        //         {
-        //             size_t actors_count_in_slot = courses_[course_index].GetActorCount(slot_index);
-        //             if (actors_count_in_slot >= setting_.actors_per_course || actors_count_in_slot == 0)
-        //             {
-        //                 break;
-        //             }
-        //             size_t other_actors_count_in_slot = courses_[other_course_index].GetActorCount(slot_index);
-        //             size_t summarized_actor_count = actors_count_in_slot + other_actors_count_in_slot;
-        //             if (summarized_actor_count <= setting_.actors_per_course && other_actors_count_in_slot > 0)
-        //             {
-        //                 std::list<Actor *> course_group = courses_[other_course_index].ClearSlot(slot_index);
-        //                 courses_[course_index].AddToSlot(course_group, slot_index);
-        //             }
-        //         }
-        //     }
-        // }
+        for (size_t course_index = 0; course_index < course_count; ++course_index)
+        {
+            for (size_t slot_index = 0; slot_index < slot_count_per_week; ++slot_index)
+            {
+                for (size_t other_course_index = course_index + 1; other_course_index < course_count; ++other_course_index)
+                {
+                    size_t actors_count_in_slot = courses_[course_index].GetActorCount(slot_index);
+                    if (actors_count_in_slot >= setting_.actors_per_course || actors_count_in_slot == 0)
+                    {
+                        break;
+                    }
+                    size_t other_actors_count_in_slot = courses_[other_course_index].GetActorCount(slot_index);
+                    size_t summarized_actor_count = actors_count_in_slot + other_actors_count_in_slot;
+                    if (summarized_actor_count <= setting_.actors_per_course && other_actors_count_in_slot > 0)
+                    {
+                        std::list<Actor *> course_group = courses_[other_course_index].ClearSlot(slot_index);
+                        courses_[course_index].AddToSlot(course_group, slot_index);
+                    }
+                }
+            }
+        }
     }
 
     void School::SimulateDays(size_t days)
