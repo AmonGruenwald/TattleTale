43bd5e7ae5f3717ca8aa9b8c9392669b050ff7b7
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-03-29 21:29:57 +0200

    Update documentation for tale.hpp

--- source/tale/tale.hpp
+++ source/tale/tale.hpp
@@ -1,4 +1,4 @@
-/// @file tale.hpp Collects everything that is necessary so user only have to include this one file.
+/// @file tale.hpp Access point for the simulation side of the project.
 #ifndef TALE_TALE_H
 #define TALE_TALE_H
 #include "shared/kernels/kernel.hpp"
@@ -19,11 +19,17 @@
 #include "tale/school.hpp"
 #include "tale/course.hpp"
 /**
- * @brief Responsible for the event generation of the TattleTale project.
+ * @brief Namespace for the TattleTale project.
  */
 namespace tattletale
 {
-    // TODO: should return events and needs to be parameterizable
+    /**
+     * @brief Run the whole simulation.
+     *
+     * @param chronicle Chronicle object where Kernels get written to. Allows sharing of events with the curation part.
+     * @param random Random object that is to be used by the simulation. This allows repeatability.
+     * @param setting Contains all parameters used by the simulation.
+     */
     void Tale(Chronicle &chronicle, Random &random, const Setting &setting);
 } // namespace tattletale
 #endif // TALE_TALE_H
