ade4e590bb72e99be81bb93ca9ecec0fbb3af159
Amon Ahmed <Amon.Shokhin.Ahmed@gmail.com>
2022-03-29 21:30:04 +0200

    Update documentation for tattle.hpp

--- source/tattle/tattle.hpp
+++ source/tattle/tattle.hpp
@@ -3,14 +3,15 @@
 #define TATTLE_TATTLE_H
 #include "shared/chronicle.hpp"
 #include "shared/setting.hpp"
-/**
-#include "shared/chronicle.hpp"
-#include "shared/setting.hpp"
- * @brief Responsible for the curation of the TattleTale project.
- * For now it just contains two basic functions for testing purposes.
- */
+
 namespace tattletale
 {
+    /**
+     * @brief Run a curation on the \link Kernel Kernels \endlink stored in the passed Chronicle.
+     *
+     * @param chronicle Chronicle object where \link Kernel Kernels \endlink are stored.
+     * @param setting Contains all parameters used by the curation.
+     */
     std::string Tattle(const Chronicle &chronicle, const Setting &setting);
 } // namespace tattletale
 #endif // TATTLE_TATTLE_H
