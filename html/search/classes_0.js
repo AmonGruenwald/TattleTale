2f805d6036350382591a5b5777f5e65a10b31c37
AmonShokhinAhmed <32668336+AmonShokhinAhmed@users.noreply.github.com>
2022-06-13 16:42:46 +0000

    Deploying to gh-pages from @ AmonShokhinAhmed/TattleTale@1de3ac0071981edd1fdf8f506c14ff38438fd3aa 🚀

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['absoluteinterestcuration_0',['AbsoluteInterestCuration',['../classtattletale_1_1_absolute_interest_curation.html',1,'tattletale']]],
-  ['actor_1',['Actor',['../classtattletale_1_1_actor.html',1,'tattletale']]]
+  ['actor_1',['Actor',['../classtattletale_1_1_actor.html',1,'tattletale']]],
+  ['actorstatus_2',['ActorStatus',['../structtattletale_1_1_actor_status.html',1,'tattletale']]]
 ];
